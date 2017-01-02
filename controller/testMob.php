0846b71e983b21176540081233d2712f5dff64ac
Michael Hong <idea_hong@163.com>
2017-01-02 21:52:05 +0800

    update testMob

--- controller/testMob.php
+++ controller/testMob.php
@@ -1,4 +1,19 @@
 <?php
-print_r("test_success\n");
-print_r($_POST);
+use \Tuanduimao\Loader\App as App;
+use \Tuanduimao\Utils as Utils;
+use \Tuanduimao\Tuan as Tuan;
+use \Tuanduimao\Excp as Excp;
+use \Tuanduimao\Conf as Conf;
+use \Tuanduimao\Wechat as Wechat;
+
+class DefaultsController extends \Tuanduimao\Loader\Controller {
+	
+	function __construct() {
+	}
+
+	function test() {
+		print_r("test_success\n");
+		print_r($_POST);
+	}
+}
 ?>
